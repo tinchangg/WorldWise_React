dbac6830617f34ae4f5937f8456fc07abb481709
Chang <205607136+tinchangg@users.noreply.github.com>
2025-11-03 15:54:57 +0800

    Modify npm script, env vars for production

--- server/db.js
+++ server/db.js
@@ -1,13 +1,5 @@
 import pg from "pg";
 
-// DEV ONLY
-console.log("DB Config:", {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
-
 const { Pool } = pg;
 
 const db = new Pool({
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
