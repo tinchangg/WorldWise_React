4e920cf1d8b50f9885479d354252161fe49b1814
Chang <205607136+tinchangg@users.noreply.github.com>
2026-03-26 20:37:28 +0800

    refactor: streamline Google OAuth logic and enhance user service functions
    - Add update last login to oauth logic

--- server/routes/oauthRoutes.js
+++ server/routes/oauthRoutes.js
@@ -1,88 +1,25 @@
 import express from "express";
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import db from "../db.js";
 import { createToken, setCookieToken } from "../services/tokenService.js";
+import {
+  updateLastLogin,
+  checkExistingEmail,
+  checkLinkedAccount,
+  createUserWithoutEmail,
+  linkUserByEmail,
+} from "../services/userService.js";
 
 const router = express.Router();
 
-const AVATAR_FALLBACK = "https://i.pravatar.cc/100?u=zz";
-
 // HELPERS
-async function checkLinkedAccount(provider, id) {
-  const result = await db.query(
-    "SELECT user_id FROM users_linked_accounts WHERE provider_name = $1 AND provider_id = $2;",
-    [provider, id],
-  );
-
-  // No account found
-  if (result.rows.length === 0) {
-    return null;
-  }
-
-  return result.rows[0].user_id;
-}
-
 function extractVerifiedEmails(emailArr) {
   return emailArr
     .filter((emailObj) => emailObj.verified === true)
     .map((emailObj) => emailObj.value);
 }
 
-async function checkExistingEmail(emailArr) {
-  const result = await db.query(
-    "SELECT id, email FROM users WHERE email = ANY($1::text[]);",
-    [emailArr],
-  );
-
-  return result.rows;
-}
-
-async function CreateUserWithoutEmail(profileData) {
-  const avatar = profileData.photos?.[0]?.value || AVATAR_FALLBACK;
-
-  // create user
-  const result = await db.query(
-    "INSERT INTO users(name, avatar) VALUES ($1, $2) RETURNING *;",
-    [profileData.displayName, avatar],
-  );
-  const user = result.rows[0];
-
-  // create linked accout
-  try {
-    await db.query(
-      "INSERT INTO users_linked_accounts(user_id, provider_name, provider_id, raw_email) VALUES ($1, $2, $3, $4);",
-      [
-        user.id,
-        profileData.provider,
-        profileData.id,
-        JSON.stringify(profileData.emails),
-      ],
-    );
-
-    return user.id;
-  } catch (err) {
-    // clean up on linked account creation failure
-    await db.query("DELETE FROM users WHERE id = $1;", [user.id]);
-    throw err;
-  }
-}
-
-async function LinkUserByEmail(userData, profileData) {
-  const result = await db.query(
-    "INSERT INTO users_linked_accounts(user_id, provider_name, provider_id, linked_email, raw_email) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
-    [
-      userData.id,
-      profileData.provider,
-      profileData.id,
-      userData.email,
-      JSON.stringify(profileData.emails),
-    ],
-  );
-
-  return result.rows[0].user_id;
-}
-
+// Google OAuth Strategy
 passport.use(
   new GoogleStrategy(
     {
@@ -101,6 +38,7 @@ passport.use(
 
         // 1) User existing
         if (userId) {
+          await updateLastLogin(userId);
           return done(null, userId);
         }
 
@@ -109,7 +47,7 @@ passport.use(
 
         // A) No Verified Email -> create account without email (only enable oauth)
         if (verifiedEmails.length === 0) {
-          const newUserId = await CreateUserWithoutEmail(profile);
+          const newUserId = await createUserWithoutEmail(profile);
           return done(null, newUserId);
         }
 
@@ -118,13 +56,14 @@ passport.use(
 
         // a) no match -> create account without email (only enable oauth)
         if (userArr.length === 0) {
-          const newUserId = await CreateUserWithoutEmail(profile);
+          const newUserId = await createUserWithoutEmail(profile);
           return done(null, newUserId);
         }
 
         // b) single match -> link user
         if (userArr.length === 1) {
-          const linkedUserId = await LinkUserByEmail(userArr[0], profile);
+          const linkedUserId = await linkUserByEmail(userArr[0], profile);
+          await updateLastLogin(linkedUserId);
           return done(null, linkedUserId);
         }
 
--- server/services/userService.js
+++ server/services/userService.js
@@ -1,7 +1,77 @@
 import db from "../db.js";
 
+const AVATAR_FALLBACK = "https://i.pravatar.cc/100?u=zz";
+
 export async function updateLastLogin(userId) {
   return db.query("UPDATE users SET last_login = NOW() WHERE id = $1", [
     userId,
   ]);
+}
+
+export async function checkLinkedAccount(provider, id) {
+  const result = await db.query(
+    "SELECT user_id FROM users_linked_accounts WHERE provider_name = $1 AND provider_id = $2;",
+    [provider, id],
+  );
+
+  // No account found
+  if (result.rows.length === 0) {
+    return null;
+  }
+
+  return result.rows[0].user_id;
+}
+
+export async function checkExistingEmail(emailArr) {
+  const result = await db.query(
+    "SELECT id, email FROM users WHERE email = ANY($1::text[]);",
+    [emailArr],
+  );
+
+  return result.rows;
+}
+
+export async function createUserWithoutEmail(profileData) {
+  const avatar = profileData.photos?.[0]?.value || AVATAR_FALLBACK;
+
+  // create user
+  const result = await db.query(
+    "INSERT INTO users(name, avatar) VALUES ($1, $2) RETURNING *;",
+    [profileData.displayName, avatar],
+  );
+  const user = result.rows[0];
+
+  // create linked account
+  try {
+    await db.query(
+      "INSERT INTO users_linked_accounts(user_id, provider_name, provider_id, raw_email) VALUES ($1, $2, $3, $4);",
+      [
+        user.id,
+        profileData.provider,
+        profileData.id,
+        JSON.stringify(profileData.emails),
+      ],
+    );
+
+    return user.id;
+  } catch (err) {
+    // clean up on linked account creation failure
+    await db.query("DELETE FROM users WHERE id = $1;", [user.id]);
+    throw err;
+  }
+}
+
+export async function linkUserByEmail(userData, profileData) {
+  const result = await db.query(
+    "INSERT INTO users_linked_accounts(user_id, provider_name, provider_id, linked_email, raw_email) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
+    [
+      userData.id,
+      profileData.provider,
+      profileData.id,
+      userData.email,
+      JSON.stringify(profileData.emails),
+    ],
+  );
+
+  return result.rows[0].user_id;
 }
